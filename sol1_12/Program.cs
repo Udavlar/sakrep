be9c8d0c35c82bd6d6f8d73426d06e0fee2481cc
Larisa <udavlar@mail.ru>
2022-09-18 21:21:59 +0500

    вывести 2-е число в трехзначном

---
+++ sol1_12/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.Write("Введите трехзначное число: ");
+int num=int.Parse(Console.ReadLine());
+if (num < 100 || num > 999){
+    Console.WriteLine("Неверное число");
+    return;
+}
+int y=num%100;
+int x = y / 10;
+Console.WriteLine($"{x}");
