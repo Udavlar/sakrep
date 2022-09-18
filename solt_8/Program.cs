ccce47ddaeb6813b36bb16d55d70fddf0b299415
Larisa <udavlar@mail.ru>
2022-09-18 21:02:48 +0500

    квадрат

---
+++ solt_8/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int sqr = number * number;
+Console.Write($"Квадрат числа {number} равняется {sqr} ");
+int sqr1 = Convert.ToInt32(Math.Pow(number, 6));
+Console.Write($" Квадрат числа {number} равняется {sqr1}");
