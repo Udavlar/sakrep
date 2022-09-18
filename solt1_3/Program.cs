dfba9bf4342892234f5e5117d034c5f2b2948655
Larisa <udavlar@mail.ru>
2022-09-18 14:04:08 +0500

    выборочные числа сложения

--- solt1_3/Program.cs
+++ solt1_3/Program.cs
@@ -1,4 +1,6 @@
-﻿int numberA = 31;
-int numberВ = 55;
+﻿int numberA =new Random().Next(1,10);// 1 2 3 4 ... 9
+Console.WriteLine(numberA);
+int numberВ = new Random().Next(1,10);
+Console.WriteLine(numberВ);
 int result = numberA + numberВ;
 Console.WriteLine(result);
