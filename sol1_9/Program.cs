d40f8a16c109ffc1e50b2fe3e5a447f36f288cbb
Larisa <udavlar@mail.ru>
2022-09-18 21:08:36 +0500

    проверка квадрат  числа в число а

---
+++ sol1_9/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите A: ");
+int number1=int.Parse(Console.ReadLine());
+Console.Write("Введите B: ");
+int number2=int.Parse(Console.ReadLine());
+
+if(number2*number2==number1){
+    Console.WriteLine("A является квадратом B");
+    }
+else{
+    Console.WriteLine("A не является квадратом B");}
